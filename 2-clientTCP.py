428bc2c06d530d941985bca9c12e8bf8a1962033
Sofia Recreio <sofia.recreio2@gmail.com>
2025-04-24 17:40:32 -0300

    add comments on clientTCP

--- 2-clientTCP.py
+++ 2-clientTCP.py
@@ -1,16 +1,16 @@
 from socket import *
 
-meuHost = '127.0.0.1'
-minhaPorta = 5001
+meuHost = '127.0.0.1'  # Endereço IP local
+minhaPorta = 5001  # Porta do servidor
 
-sockobj = socket(AF_INET, SOCK_STREAM)
+sockobj = socket(AF_INET, SOCK_STREAM)  # Cria socket TCP
 dest = (meuHost, minhaPorta)
-sockobj.connect(dest)
+sockobj.connect(dest)  # Estabelece conexão TCP com o servidor
 
 print('Para sair use CTRL+X\n')
 msg = ''
-while msg != '\x18':
-    msg = input()
-    sockobj.send(msg.encode())
+while msg != '\x18':  # Enquanto a mensagem não for Ctrl+X
+    msg = input()  # Lê entrada do usuário
+    sockobj.send(msg.encode())  # Envia a mensagem ao servidor
 
-sockobj.close()
+sockobj.close()  # Fecha a conexão
