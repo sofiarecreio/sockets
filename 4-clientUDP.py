11fa14dc835a72aa80b650cda4ed7bb0b23bc75f
Sofia Recreio <sofia.recreio2@gmail.com>
2025-04-24 17:38:33 -0300

    add comment on clientUDP

--- 4-clientUDP.py
+++ 4-clientUDP.py
@@ -1,15 +1,18 @@
-from socket import *
+from socket import *  # Importa funções da biblioteca socket
 
-meuHost = '127.0.0.1'
-minhaPorta = 5000
+meuHost = '127.0.0.1'  # Endereço IP do servidor (localhost)
+minhaPorta = 5000  # Porta onde o servidor está escutando
 
-sockobj = socket(AF_INET, SOCK_DGRAM)
-dest = (meuHost, minhaPorta)
+sockobj = socket(AF_INET, SOCK_DGRAM)  # Cria um socket UDP
 
-print('Para sair use CTRL+X\n')
-msg = ''
-while msg != '\x18':
-    msg = input()
-    sockobj.sendto(msg.encode(), dest)
+dest = (meuHost, minhaPorta)  # Define o endereço de destino (host, porta)
 
-sockobj.close()
+print('Para sair use CTRL+X\n')  # Mensagem para o usuário
+msg = ''  # Inicializa a variável da mensagem
+
+# Loop principal do cliente
+while msg != '\x18':  # Envia mensagens até o usuário digitar Ctrl+X
+    msg = input()  # Lê entrada do usuário
+    sockobj.sendto(msg.encode(), dest)  # Envia a mensagem codificada (em bytes) para o servidor
+
+sockobj.close()  # Fecha o socket e encerra o cliente
